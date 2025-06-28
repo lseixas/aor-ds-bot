c7287e687a0a8320a02bf661a68fbc16719b283c
lseixas <lseixas.iorio@gmail.com>
2025-06-27 21:04:06 -0300

    attempting to simplify

--- iac/main.tf
+++ iac/main.tf
@@ -22,21 +22,21 @@ resource "render_background_worker" "resource-aor-ds-bot" {
 
   start_command = "python -m app.main"
 
+  env_vars = {
+    "DISCORD_TOKEN" = var.DISCORD_TOKEN
+    "APP_ID"        = var.APP_ID
+    "PUBLIC_KEY"    = var.PUBLIC_KEY
+    "GUILD_ID"      = var.GUILD_ID
+  }
+
   runtime_source = {
 
-    env_vars = {
-      "DISCORD_TOKEN" = var.DISCORD_TOKEN
-      "APP_ID"        = var.APP_ID
-      "PUBLIC_KEY"    = var.PUBLIC_KEY
-      "GUILD_ID"      = var.GUILD_ID
-    }
-
-    native_runtime = {
       auto_deploy   = true
       branch        = "main"
       build_command = "pip install -r requirements.txt"
       repo_url      = "https://www.github.com/lseixas/aor-ds-bot"
       runtime       = "python"
-    }
+  
   }
+  
 }
